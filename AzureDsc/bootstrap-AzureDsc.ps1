78c7f71c6ea864a537a0f87610171d2d8b8308bb
Clay Haertzen <clay.haertzen@e-hps.com>
2017-12-01 10:52:21 -0500

    implemented initial (untested) bootstrap for AzureDsc

--- AzureDsc/bootstrap-AzureDsc.ps1
+++ AzureDsc/bootstrap-AzureDsc.ps1
@@ -1,1 +1,24 @@
-https://www.microsoft.com/en-us/download/confirmation.aspx?id=54616
+[cmdletbinding()]
+Param(
+    [string[]]$computername = $env:computername,
+    [System.Management.Automation.PSCredential]$credential
+)
+Process{
+    #set up some splats
+    $sessionParams = @{Computername = $computername}
+    if($credential){$sessionParams.Add('Credential',$credential)}
+
+    #build the metaConfigurations
+    . "$psscriptroot\build-DscMetaConfiguration.ps1" -computername $computername
+    
+    #verify that each server has WMF5.1 installed
+    $PsSession = new-pssession @sessionParams
+    invoke-command $PsSession -scriptblock {
+        if($PSVersionTable.PSVersion.Major -lt 5){
+            invoke-webrequest -uri "https://www.microsoft.com/en-us/download/confirmation.aspx?id=54616" -outfile $env:temp\WMF_Install.msi
+        }
+    }
+
+    #push the DscConfiguration
+    set-DscLocalConfigurationManager -path $env:UserProfile\Desktop\DscMetaConfigs
+}
